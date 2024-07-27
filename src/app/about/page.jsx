21f329caf3b6f2b846a832c4a16bfac7c13a7f26
Joseph Liyon PT <josephliyon23@gmail.com>
2024-07-27 12:43:19 +0530

    Added About Page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptatem reiciendis quisquam accusamus laborum? Quibusdam explicabo, praesentium dignissimos at tempore quidem, pariatur facilis quos animi eum, ipsam fugiat incidunt laudantium!
+
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur culpa nisi quam voluptas placeat et pariatur unde perspiciatis ipsum ex qui, obcaecati expedita cum voluptates perferendis sunt aliquid! Doloribus, cupiditate!
+
+        </p>
+    </div>
+  )
+}
+
+export default About
